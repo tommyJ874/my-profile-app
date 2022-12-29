f82c93cadd92dba8d5d7167ba0491c5edc685057
tommyJ <tommyJ.dev@gmail.com>
2022-12-29 23:58:46 +0900

    Fix 사이드 메뉴 애니메이션 적용

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,8 @@
-import { Mail } from '@vectopus/atlas-icons-react';
 import kakaoTalkIcon from '../styles/images/kakaoTalk.png';
 import githubIcon from '../styles/images/github-90.png';
 import gmailIcon from '../styles/images/gmail-100.png';
 import React, { useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 
 const Header = () => {
   const [isShown, setIsShown] = useState(false);
@@ -60,7 +60,7 @@ const Header = () => {
             className='transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-110'
           >
             <img
-              class='w-7 h-7'
+              className='w-7 h-7'
               src={kakaoTalkIcon}
               alt='kakaoTalk'
               title='kakaoTalk'
@@ -73,7 +73,12 @@ const Header = () => {
               rel='noreferrer'
               className=''
             >
-              <img class='w-7 h-7' src={gmailIcon} alt='gmail' title='gmail' />
+              <img
+                className='w-7 h-7'
+                src={gmailIcon}
+                alt='gmail'
+                title='gmail'
+              />
             </a>
           </div>
           <a
@@ -82,7 +87,12 @@ const Header = () => {
             rel='noreferrer'
             className='transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-110'
           >
-            <img class='w-8 h-8' src={githubIcon} alt='github' title='github' />
+            <img
+              className='w-8 h-8'
+              src={githubIcon}
+              alt='github'
+              title='github'
+            />
           </a>
           {/* <div class='inline-flex overflow-hidden relative justify-center items-center w-10 h-10 bg-gray-200 rounded-full'></div>
           <div class='inline-flex overflow-hidden relative justify-center items-center w-10 h-10 bg-gray-200 rounded-full'>
@@ -93,14 +103,42 @@ const Header = () => {
       </div>
 
       {/* mobile menu items */}
-      <div className={`md:hidden ${!isShown && 'hidden'}`}>
-        <a href='#' className='block py-2 px-4 text-sm hover:bg-gray-200'>
-          Pricing
-        </a>
-        <a href='#' className='block py-2 px-4 text-sm hover:bg-gray-200'>
-          Features
-        </a>
-      </div>
+      <AnimatePresence>
+        {isShown && (
+          <motion.div
+            className={`md:hidden bg-gray-400 absolute w-1/2 h-full top-0 left-0 flex flex-col items-start p-5 space-y-5`}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <button onClick={toggleHandler} className='my-1'>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                strokeWidth={1.5}
+                stroke='currentColor'
+                className='w-6 h-6 m-0'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  d='M3.75 9h16.5m-16.5 6.75h16.5'
+                />
+              </svg>
+            </button>
+            <a href='#' className='text-sm self-stretch'>
+              About Me
+            </a>
+            <a href='#' className='text-sm self-stretch'>
+              Portfolio
+            </a>
+            <a href='#' className='text-sm self-stretch'>
+              Cooperation
+            </a>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </nav>
   );
 };
