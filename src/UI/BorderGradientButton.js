fafae6891b2f1f7e58be37ea27a6e6f3f2126907
tommyJ874 <tommyJ.dev@gmail.com>
2023-01-06 09:48:16 +0900

    Fix 모바일 사이즈 수정

--- src/UI/BorderGradientButton.js
+++ src/UI/BorderGradientButton.js
@@ -4,7 +4,7 @@ const BorderGradientButton = (props) => {
   return (
     <button className='h-8 w-24 rounded-lg shadow-md hover:shadow-lg shadow-gray-300 border bg-gradient-to-tr from-gradient-from via-gradient-via to-gradient-to p-[0.08rem]'>
       <div className='flex h-full w-full rounded-md items-center justify-center bg-white back'>
-        <span className='text-[0.2rem] font-bold text-gradient-via'>
+        <span className='text-[0.4rem] font-bold text-gradient-via'>
           {props.children}
         </span>
       </div>
--- src/UI/MakePDFButton.js
+++ src/UI/MakePDFButton.js
@@ -15,7 +15,7 @@ const makePDFButton = (props) => {
       className='h-8 w-44 rounded-lg shadow-md hover:shadow-lg shadow-gray-300 border bg-gradient-to-tr from-gradient-from via-gradient-via to-gradient-to'
     >
       <div className='flex h-full w-full justify-center items-center'>
-        <span className='text-[0.2rem] font-bold text-white'>
+        <span className='text-[0.4rem] font-bold text-white'>
           {props.children}
         </span>
       </div>
